666f71178852352ab6e3104b86c99b0ec55046af
peachapp <571596435@qq.com>
2022-07-22 17:16:21 +0800

    add qrcode

--- src/views/test/index.js
+++ src/views/test/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { signIn, getUser } from "api/login";
 import PassInput from "components/passInput";
+import Qrcode from "components/qrcode";
 
 function Test() {
   const fn = async () => {
@@ -16,6 +17,12 @@ function Test() {
     <div id="Test" className="test-container">
       test
       <PassInput width="280px" value={a} onChange={(v) => setA(v)} />
+      <Qrcode
+        value={a}
+        width="200px"
+        timeout={10000}
+        refresh={() => setA(a + "1")}
+      />
     </div>
   );
 }
