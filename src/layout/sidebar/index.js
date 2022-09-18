5a9458092dc669aba1ef10b43fa3e663972b9efe
taotao <571596435@qq.com>
2022-09-18 22:47:20 +0800

    fix Sidebar

--- src/layout/sidebar/index.js
+++ src/layout/sidebar/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Layout, Menu } from "antd";
-import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { Icon } from "@iconify-icon/react";
 import { useRecoilValue } from "recoil";
 import { collapsedStore } from "store";
 import { autoPrefix } from "utils";
@@ -11,16 +11,28 @@ const cx = autoPrefix(styles.prefix);
 
 const menuData = [
   {
-    key: "dashboard/workbench",
-    title: "工作台",
-    path: "/dashboard/workbench",
-    icon: <HomeOutlined />,
+    key: "dashboard",
+    title: "dashboard",
+    path: "dashboard",
+    icon: "ant-design:appstore-outlined",
+    children: [
+      {
+        key: "dashboard/workbench",
+        title: "工作台",
+        path: "/dashboard/workbench",
+      },
+      {
+        key: "dashboard/analysis",
+        title: "分析页",
+        path: "/dashboard/analysis",
+      },
+    ],
   },
   {
     key: "aboutAuthor",
     title: "关于作者",
     path: "/aboutAuthor",
-    icon: <UserOutlined />,
+    icon: "icon-park-outline:me",
   },
   // { key: '404', title: '404', path: '', icon: '' },
 ];
@@ -44,10 +56,31 @@ const Sidebar = () => {
     setSelectedKeys([key]);
   };
 
+  const getmenuDataItem = (list) => {
+    return (list || []).map((item) => {
+      if (Array.isArray(item.children)) {
+        return (
+          <Menu.SubMenu
+            key={item.key}
+            icon={<Icon icon={item.icon} width={20} />}
+            title={item.title}
+          >
+            {getmenuDataItem(item.children)}
+          </Menu.SubMenu>
+        );
+      }
+      return (
+        <Menu.Item key={item.key} icon={<Icon icon={item.icon} width={20} />}>
+          {item.title}
+        </Menu.Item>
+      );
+    });
+  };
+
   return (
     <Layout.Sider
       className={cx("container")}
-      width={144}
+      width={210}
       collapsedWidth={64}
       trigger={null}
       collapsible
@@ -59,13 +92,7 @@ const Sidebar = () => {
         selectedKeys={selectedKeys}
         onClick={onSelectedKeysChange}
       >
-        {(menuData || []).map((menuDataItem) => {
-          return (
-            <Menu.Item key={menuDataItem.key} icon={menuDataItem.icon}>
-              {menuDataItem.title}
-            </Menu.Item>
-          );
-        })}
+        {getmenuDataItem(menuData)}
       </Menu>
     </Layout.Sider>
   );
